6d262377fccb3cf3cfe324918d39ee382b3edf4c
fackloar <fackloar@gmail.com>
2022-11-08 05:26:15 +0300

    h/w 2
    1. Добавить в компонент App поле стейта messageList. В нем хранить массив объектов -
    сообщений (объект должен содержать, как минимум, поля text и author). Начальное
    значение - пустой массив).
    2. Рендерить список сообщений через map.
    © geekbrains.ru 19
    3. Добавить и стилизовать форму - поле для ввода текста и кнопка для отправки сообщения.
    При отправке сообщения обновление UI должно происходить за счет обновления стейта
    App.
    4. Добавить в App useEffect - на каждое отправленное пользователем сообщение должен
    отвечать робот (должно автоматически отправляться сообщение с фиксированным
    текстом) - для этого необходимо проверять автора последнего сообщения.
    5. * Сделать задержку ответа робота - сообщение от него должно отправляться через
    некоторый промежуток времени после отправки сообщения пользователя (напр., 1.5 сек).

--- react-gb/src/App.js
+++ react-gb/src/App.js
@@ -1,18 +1,63 @@
 import './App.css';
-import Message from './components/Message.jsx'
 import logo from './logo512.png';
+import { useState, useEffect, useCallback } from "react";
+import MessageList from "./components/MessageList/MessageList.jsx";
 
-function App(props) {
-  return (
-    <div>
-      <header className='App-header'>
-      <img src={logo} className="App-logo" alt="logo" />
-        Welcome!
-        <Message message = {props.message}/>
-      </header>
-    </div>
+const emptyMessage = { author: "", body: "" };
+const botMessage = { author: "Bot", body: "" };
+
+function App() {
+
+  const [currentUser, setCurrentUser] = useState("Username");
+  const [messageList, setMessageList] = useState([]);
+  const [idCounter, setIdCounter] = useState(0);
+  const [formMessage, setFormMessage] = useState({ ...emptyMessage });
+
+  const getNewId = useCallback(() => {
+    const newId = idCounter + 1;
+    setIdCounter(newId);
+    return newId;
+  }, [idCounter]);
+
+  const addMessage = useCallback(
+    (newMessage) => {
+      setMessageList([...messageList, { ...newMessage, id: getNewId() }]);
+    },
+    [getNewId, messageList]
   );
 
+  useEffect(() => {
+    if (messageList.length === 0) return;
+    const lastMessage = messageList.at(-1);
+    if (lastMessage.author === botMessage.author) return;
+    setTimeout(() => addMessage({ ...botMessage, body: `${lastMessage.author} said something` }), 1500);
+  }, [messageList, addMessage]);
+
+  const addMessageFromForm = (e) => {
+    e.preventDefault();
+    addMessage(formMessage);
+    setFormMessage({ ...emptyMessage });
+  };
+
+return (
+  <div className="App">
+
+      <div className="chat">
+        <div className="chat-user">
+          <label>Current User: </label>
+          <input type="text" value={currentUser} onChange={(e) => setCurrentUser(e.target.value)} />
+        </div>
+
+        <MessageList messages={messageList} currentUser={currentUser} />
+
+        <form className="form-message">
+          <input type="text" placeholder="user" value={formMessage.author} onChange={(e) => setFormMessage({ ...formMessage, author: e.target.value })} />
+          <input type="text" placeholder="message" value={formMessage.body} onChange={(e) => setFormMessage({ ...formMessage, body: e.target.value })} />
+          <button onClick={addMessageFromForm}>send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
